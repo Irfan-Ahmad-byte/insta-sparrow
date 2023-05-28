97d399ca7ecac9cdcbc0caed7c02990b3b48f3c4
irfan-ahmad-byte <irfan.ahmad.mlka@gmail.com>
2023-05-28 12:58:08 +0500

    compose

--- send_message_sel.py
+++ send_message_sel.py
@@ -32,7 +32,7 @@ class InstagramBot:
         # Pass the session_id to the ChromeOptions constructor
         #chrome_options.add_argument('--session-id=%s' % session_id)
 
-        self.driver = webdriver.Remote(command_executor='http://134.209.208.89:4444', options=chrome_options)
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444', options=chrome_options)
         #self.driver = uc.Chrome(options)
 
     def login(self):
