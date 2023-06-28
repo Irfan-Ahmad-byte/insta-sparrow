20e2212cdd151827e9b05840bdabc72d31f9c530
irfan-ahmad-byte <irfan.ahmad.mlka@gmail.com>
2023-06-28 22:38:09 +0500

    user box xpath

--- send_message_sel.py
+++ send_message_sel.py
@@ -169,7 +169,7 @@ class InstagramBot:
                     if usernames in btn.text:
                         time.sleep(random.uniform(2, 5))'''
                 try:
-                     user_select.click()
+                     user_select.find_elements(By.TAG_NAME, "//div")[0].click()
                     #btn.click()
                      #break
                 except Exception as e:
