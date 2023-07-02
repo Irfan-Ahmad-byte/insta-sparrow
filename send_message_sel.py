90875f61c264b6beeb00128f18612e6de979f892
irfan-ahmad-byte <irfan.ahmad.mlka@gmail.com>
2023-07-02 18:04:02 +0500

    use action chains

--- send_message_sel.py
+++ send_message_sel.py
@@ -169,14 +169,14 @@ class InstagramBot:
                 queryBox.send_keys(u)
             user_pop = self.driver.find_element(By.XPATH, "//div[@role='dialog']")
             try:
-                user_select = user_pop.find_elements(By.XPATH, "//div[@role='button']")
+                user_select = user_pop.find_elements(By.CLASS_NAME, "x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x1dm5mii x16mil14 xiojian x1yutycm x1lliihq x193iq5w xh8yej3")
                 for btn in user_select:
                     if usernames in btn.text:
                         another_btn = btn.find_elements(By.TAG_NAME, "div")
                         try:
-                            #ActionChains(self.driver).move_to_element(btn).perform()
-                            #ActionChains(self.driver).click(btn).perform()
-                            btn.click()
+                            ActionChains(self.driver).move_to_element(btn).perform()
+                            ActionChains(self.driver).click(btn).perform()
+                            #btn.click()
                             break
                         except Exception as e:
                             print(e)
@@ -192,9 +192,9 @@ class InstagramBot:
         try:
             for btn in next_btn:
                 if btn.text == 'Chat' or btn.text == 'Next':
-                    #ActionChains(self.driver).move_to_element(btn).perform()
-                    #ActionChains(self.driver).click(btn).perform()
-                    btn.click()
+                    ActionChains(self.driver).move_to_element(btn).perform()
+                    ActionChains(self.driver).click(btn).perform()
+                    #btn.click()
                     break
         except:
             try:
